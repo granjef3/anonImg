4eb536a753792429f6220ec26fa1084e63279f6b
iD Student <Student@idtech-id08.princeton.edu>
2012-07-02 12:18:52 -0400

    Initial Commit

---
+++ AnonImgTests/AnonImgTests.h
@@ -0,0 +1,13 @@
+//
+//  AnonImgTests.h
+//  AnonImgTests
+//
+//  Created by iD Student on 7/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AnonImgTests : SenTestCase
+
+@end
---
+++ AnonImgTests/AnonImgTests.m
@@ -0,0 +1,32 @@
+//
+//  AnonImgTests.m
+//  AnonImgTests
+//
+//  Created by iD Student on 7/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AnonImgTests.h"
+
+@implementation AnonImgTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in AnonImgTests");
+}
+
+@end
